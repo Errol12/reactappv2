4f9f396f3f976859a7c534ce9d6567604dd6a222
Errol Fernandes <errol.fernandes@atos.net>
2019-02-25 16:18:42 +0530

    Props and state Nested Components Signed-off-by: Errol Fernandes <errol.fernandes@atos.net>

---
+++ src/Ninjas.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Ninjas extends Component{
+    render(){
+        //console.log(this.props);
+        const { name,age,belt } = this.props;
+        return (
+            <div className="ninja">
+                <div>Name: {name}</div>
+                <div>Age: {this.props.age}</div>
+                <div>Belt: {this.props.belt}</div>
+            </div>
+        )
+    }
+}
+
+export default Ninjas;
