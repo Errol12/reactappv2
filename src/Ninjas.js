78db76880b7e2e2e92972e2fefc67b13e4e5850c
Errol Fernandes <errol.fernandes@atos.net>
2019-02-27 15:55:38 +0530

    Delete an item from list
    Signed-off-by: Errol Fernandes <errol.fernandes@atos.net>

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Ninjas = ({ ninjas }) => {
-
-        const ninjaList = ninjas.map(ninja => {
-        if(ninja.age > 20)
+const Ninjas = ({ ninjas, deleteNinja }) => {
+    const ninjaList = ninjas.map(ninja => {
+        if(ninja.age < 200)
         {
             return (
                 <div className="ninja" key={ninja.id}>
                 <div>Name: {ninja.name}</div>
                 <div>Age: {ninja.age}</div>
                 <div>Belt: {ninja.belt}</div>
+                <button onClick={() => {deleteNinja(ninja.id)}}>Delete Ninja</button>
                 </div>
             )
         }else{
